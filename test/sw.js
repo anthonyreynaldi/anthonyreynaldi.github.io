5bce1ef06bc5b6a556f67d361de1c14e072963b5
anthonyreynaldi <77012214+anthonyreynaldi@users.noreply.github.com>
2023-03-31 00:01:29 +0700

    add .

--- test/sw.js
+++ test/sw.js
@@ -3,18 +3,18 @@ self.addEventListener('install', function(event) {
       caches.open('first-app')
         .then(function(cache) {
           cache.addAll([
-            '/',
-            '/index.html',
-            '/src/css/app.css',
-            '/src/js/app.js',
-            '/manifest.json',
-            '/src/images/icons/app-icon-144x144.png',
-            '/src/images/icons/app-icon-192x192.png',
-            '/src/images/icons/app-icon-256x256.png',
-            '/src/images/icons/app-icon-384x384.png',
-            '/src/images/icons/app-icon-48x48.png',
-            '/src/images/icons/app-icon-512x512.png',
-            '/src/images/icons/app-icon-96x96.png',
+            './',
+            './index.html',
+            './src/css/app.css',
+            './src/js/app.js',
+            './manifest.json',
+            './src/images/icons/app-icon-144x144.png',
+            './src/images/icons/app-icon-192x192.png',
+            './src/images/icons/app-icon-256x256.png',
+            './src/images/icons/app-icon-384x384.png',
+            './src/images/icons/app-icon-48x48.png',
+            './src/images/icons/app-icon-512x512.png',
+            './src/images/icons/app-icon-96x96.png',
           ])
         })
     );
